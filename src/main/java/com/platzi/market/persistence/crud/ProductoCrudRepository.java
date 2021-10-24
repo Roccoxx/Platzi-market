94a44c6fa404f34f93aca85e70fd3180d2d26bda
Roccoxx <53314419+Roccoxx@users.noreply.github.com>
2021-10-23 22:48:30 -0300

    1.0.1
    Agregadas anotaciones en query methods

--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -11,6 +11,12 @@ import com.platzi.market.persistence.entity.Producto;
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer>{
 	//@Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
 	// se podria agregar lo de arriba para hacerlo de manera nativa y modificarlo a gusto
+	// https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
+	/*
+    NATIVE QUERY METHOD
+    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> getCategoriaByNombreAsc(int idCategoria);
+    */
 	List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
     Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
